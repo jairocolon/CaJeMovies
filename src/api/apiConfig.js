69b14673aa92f5759c1042dfc90cb9d617cd2efb
Jairo Colón <jaicodigital@gmail.com>
2022-01-27 14:23:30 -0500

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,12 +1,8 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
-const API_CLAVE = `${process.env.REACT_APP_API_CLAVE}`;
-
-const apiConfig = ({
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: {API_CLAVE},
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-})
+const apiConfig = {
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "901609564690fb9e0664182b3f6cff7b",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
 
 export default apiConfig;
